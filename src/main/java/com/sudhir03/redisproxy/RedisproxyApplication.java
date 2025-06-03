2cc1c431e29c3a926f561f029831a1e993103ffd
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-06-03 13:59:00 +0530

    initial commit

---
+++ src/main/java/com/sudhir03/redisproxy/RedisproxyApplication.java
@@ -0,0 +1,13 @@
+package com.sudhir03.redisproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudhir03/redisproxy/RedisproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhir03.redisproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
